2df7d4261d0e1c21153ac0390d3b365379a6ffe4
rmattila <rmattila@users.noreply.github.com>
2015-07-14 10:00:52 -0700

    load multiple files and extract data

--- vascops.py
+++ vascops.py
@@ -1,5 +1,21 @@
+files = ["1_tn.txt", "3_tn.txt"]
 
-with open('1_tn.txt','r') as f:
-    for line in f:
-        for word in line.split():
-           print(word)
+for filename in files:
+    with open(filename, 'r') as f:
+        csv_line = ""
+        for line in f:
+            index_of_equal = line.find("=")
+            
+            # Only treat lines with a "="-sign
+            if index_of_equal != -1:
+                idx = line.find("[")
+                
+                if idx == -1:
+                    data = line[index_of_equal:]
+                else:
+                    data = line[index_of_equal:idx]
+                    
+                csv_line = csv_line + data + ","
+                
+        # Print the CSV-line
+        print csv_line
